3f83d344f0061137c4bf78601ec1ebd4e2162dc3
AmeneNeon <ameneneon@gmail.com>
2024-06-08 22:14:54 +0200

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,17 +16,23 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
+    // Find user by email
     const validUser = await User.findOne({ email });
+    // If user not found, return 404 error
     if (!validUser) {
       return next(errorHandler(404, "User is not find!"));
     }
+    // Check if password is correct
     const validPassword = bcryptjs.compareSync(password, validUser.password);
+    // If password is incorrect, return 401 error
     if (!validPassword) {
       return next(errorHandler(401, "Wrong credentials!"));
     }
-
-    const token = jwt.sign({ id: validUser._id },process.env.JWT_SECRET);
-    const {password:pass,...rest}=validUser._doc
+    // Generate JWT token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // Remove password field from user object
+    const { password: pass, ...rest } = validUser._doc;
+    // Set cookie with token
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
